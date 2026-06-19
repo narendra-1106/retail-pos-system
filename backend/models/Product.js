92a012a2955935bdfd45279d55f6ec1f5d0002dd
narendra-1106 <narendra1jagtap@gmail.com>
2026-06-19 19:58:18 +0530

    ..

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -12,30 +12,3 @@ const productSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Product', productSchema);
-const mongoose = require("mongoose");
-
-const productSchema = new mongoose.Schema({
-
-  name: {
-    type: String,
-    required: true
-  },
-
-  price: {
-    type: Number,
-    required: true
-  },
-
-  quantity: {
-    type: Number,
-    required: true
-  },
-
-  category: {
-    type: String,
-    required: true
-  }
-
-});
-
-module.exports = mongoose.model("Product", productSchema);
