864bec7d93f758cd26f5a4ecf7b2cd9ce9ab655f
narendra-1106 <narendra1jagtap@gmail.com>
2026-06-22 18:21:21 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
 
   status: {
     type: String,
-    enum: ["active", "disabled"],
+    enum: ["active", "inactive", "disabled"],
     default: "active"
   },
 
